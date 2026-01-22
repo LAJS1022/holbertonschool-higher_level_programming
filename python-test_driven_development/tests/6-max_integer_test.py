8cf98aa3277fc4558db422f0c8bca78a1c9aedfa
LuisJimenez <luisssajs@gmail.com>
2026-01-22 19:11:47 +0000

    max int test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])"""
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_positive_integers(self):
+    """Test cases for max_integer function"""
+
+    def test_regular_list(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-    def test_negative_and_positive(self):
-        self.assertEqual(max_integer([-10, 5, 3, -1]), 5)
-    def test_single_element(self):
-        self.assertEqual(max_integer([42]), 42)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
     def test_empty_list(self):
         self.assertIsNone(max_integer([]))
-    def test_all_negative(self):
-        self.assertEqual(max_integer([-5, -9, -1, -3]), -1)
-    def test_repeated_max(self):
-        self.assertEqual(max_integer([2, 3, 3, 1]), 3)
-    def test_max_at_beginning(self):
-        self.assertEqual(max_integer([9, 3, 2, 1]), 9)
-    def test_max_at_end(self):
-        self.assertEqual(max_integer([1, 2, 3, 10]), 10)
+
+    def test_single_element(self):
+        self.assertEqual(max_integer([7]), 7)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([-10, -5, -20]), -5)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-10, 0, 10, 5]), 10)
+
+    def test_duplicates(self):
+        self.assertEqual(max_integer([2, 2, 2]), 2)
+
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([1.5, 2.7, 0.3]), 2.7)
+
+    def test_list_with_none(self):
+        with self.assertRaises(TypeError):
+            max_integer([None, 1, 2])
+
+    def test_non_list_argument(self):
+        with self.assertRaises(TypeError):
+            max_integer("string")
