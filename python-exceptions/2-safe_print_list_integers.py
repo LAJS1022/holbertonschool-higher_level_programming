5619ff558a7d021dfefa553f5f062b6219c77c54
LuisJimenez <luisssajs@gmail.com>
2026-01-22 16:38:49 +0000

    print li int

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,10 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
-            # Skip silently if not an integer
             continue
         except IndexError:
-            # Stop if we go out of range
             break
     print()
     return count
