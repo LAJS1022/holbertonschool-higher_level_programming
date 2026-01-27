ac0de2eae7ca9d726efa39384425df84c32deb56
LuisJimenez <luisssajs@gmail.com>
2026-01-27 18:00:05 +0000

    super square

---
+++ python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Class Square that defines a square by size and position.
+"""
+
+
+class Square:
+    """Defines a square with private size, position, validation, area, and print method"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position with validation"""
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square with the character # and position offset"""
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
