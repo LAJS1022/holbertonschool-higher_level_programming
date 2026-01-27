1fdca72438f38d448a75773b0a5a01e2762bad99
LuisJimenez <luisssajs@gmail.com>
2026-01-27 17:35:40 +0000

    class square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,7 +9,11 @@ class Square:
     """Defines a square with private size, validation, and area method"""
 
     def __init__(self, size=0):
-        self.size = size  
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     @property
     def size(self):
