2fdb583090858747cb4d626fa18b1b781e8be816
LuisJimenez <luisssajs@gmail.com>
2026-02-03 17:32:01 +0000

    inherit from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function to check subclass inheritance."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a subclass of a_class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare
