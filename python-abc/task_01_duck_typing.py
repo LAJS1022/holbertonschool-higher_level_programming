bb8680062dc5cd8ab46e606740afeac0cb168a92
LuisJimenez <luisssajs@gmail.com>
2026-02-05 17:02:22 +0000

    duck typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,56 +1,54 @@
-#!/usr/bin/python3
-"""Defines abstract Shape class and concrete Circle and Rectangle classes."""
-
+#!/usr/bin/env python3
 from abc import ABC, abstractmethod
 import math
 
 
 class Shape(ABC):
-    """Abstract base class representing a Shape."""
+    """Abstract base class for shapes"""
 
     @abstractmethod
     def area(self):
-        """Return the area of the shape."""
+        """Return the area of the shape"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Return the perimeter of the shape."""
+        """Return the perimeter of the shape"""
         pass
 
 
 class Circle(Shape):
-    """Circle class that inherits from Shape."""
+    """Circle shape"""
 
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        """Return the area of the circle."""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """Return the perimeter (circumference) of the circle."""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle class that inherits from Shape."""
+    """Rectangle shape"""
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def area(self):
-        """Return the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Print the area and perimeter of a shape (duck typing)."""
-    print("Area: " + str(shape.area()))
-    print("Perimeter: " + str(shape.perimeter()))
+    """
+    Prints area and perimeter of any object
+    that implements area() and perimeter()
+    (duck typing)
+    """
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
