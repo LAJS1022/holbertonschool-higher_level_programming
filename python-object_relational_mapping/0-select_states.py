11a9db35723d398a23a282297eaaf2f7014bf05f
LuisJimenez <luisssajs@gmail.com>
2026-03-05 18:27:32 +0000

    select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa and yes
+Lists all states from hbtn_0e_0_usa
 """
 
 import MySQLdb
@@ -9,14 +9,14 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database
+        db=db_name
     )
 
     cur = db.cursor()
